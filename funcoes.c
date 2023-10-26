3d0bca1b41a963a504023e1cb5e99328eb678f3d
Kayky Pires <125900277+KaykyyPiress@users.noreply.github.com>
2023-10-26 20:50:23 +0000

    colocando os include e os array de nome

--- funcoes.c
+++ funcoes.c
@@ -1,1 +1,13 @@
 #include "funcoes.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct Cliente {
+    char cpf[16];
+    char nome[100];
+    char tipo_de_conta[10];
+    float saldo;
+    char senha[5];
+};
+
