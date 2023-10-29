96be6233c38b35d7b655e1458532212e472cb729
Kayky Pires <125900277+KaykyyPiress@users.noreply.github.com>
2023-10-29 19:16:24 +0000

    arrumando debito

--- funcoes.c
+++ funcoes.c
@@ -280,60 +280,8 @@ void debito(char cpf[16], char senha[5]) {
     fclose(arquivo);
 }
 
-void extratos(char cpf[16], char senha[5]) {
-    FILE *arquivo_cliente;
-    FILE *arquivo_extrato;
-    struct Cliente cliente;
-
-    arquivo_cliente = fopen("Clientes.bin", "rb");
-    if (arquivo_cliente == NULL) {
-        printf("Erro ao abrir o arquivo de clientes.\n");
-        return;
-    }
-
-    arquivo_extrato = fopen("Extrato.bin", "rb");
-    if (arquivo_extrato == NULL) {
-        printf("Erro ao abrir o arquivo de extrato.\n");
-        fclose(arquivo_cliente);
-        return;
-    }
-
-    char linha[MAX_STRING];
-
-    printf("\nExtratos:\n\n");
-
-    while (fread(&cliente, sizeof(struct Cliente), 1, arquivo_cliente)) {
-        if (strcmp(cliente.cpf, cpf) == 0 && strcmp(cliente.senha, senha) == 0) {
-            while (fgets(linha, MAX_STRING, arquivo_extrato) != NULL) {
-                // Verifica se a linha contém o CPF
-                if (strstr(linha, cpf) != NULL) {
-                    // Encontra a posição do caractere '_' na linha
-                    char *underline_pos = strchr(linha, '_');
-                    if (underline_pos != NULL) {
-                        // Calcula o tamanho da substring a ser impressa
-                        size_t len_to_print = underline_pos - linha;
-                        // Imprime a parte relevante da linha
-                        printf("%.*s\n", (int)len_to_print, linha);
-                    }
-                }
-            }
-
-            fclose(arquivo_extrato);
-            fclose(arquivo_cliente);
-            return;
-        }
-    }
-
-    fclose(arquivo_cliente);
-    fclose(arquivo_extrato);
-    return;
-}
-
-void deposito(char cpf[16]){
-    //abre os arquivos
-    FILE *arquivo;
-    FILE *extrato_arquivo;
-    
+void deposito(char cpf[16]) {
+    FILE *arquivo, *extrato_arquivo;
     struct Cliente cliente;
 
     // Variáveis de data e hora
@@ -342,9 +290,60 @@ void deposito(char cpf[16]){
     char data_e_hora_em_texto[20];
     strftime(data_e_hora_em_texto, sizeof(data_e_hora_em_texto), "%d/%m/%Y %H:%M", data_hora_atual);
 
+
     // Abre o arquivo para leitura e escrita
     arquivo = fopen("Clientes.bin", "rb+");
-    //Verifica se o arquivo existe, caso nao exista exibe a mensagem de erro.
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    // Verifica se o cliente existe
+    while (fread(&cliente, sizeof(struct Cliente), 1, arquivo)) {
+        if (strcmp(cliente.cpf, cpf) == 0) {
+            float valor;
+
+            printf("Valor para Depósito: ");
+            scanf("%f", &valor);
+
+            // Deposita na conta do cliente
+            cliente.saldo += valor;
+
+            // Faz alteração no arquivo
+            fseek(arquivo, -sizeof(struct Cliente), SEEK_CUR);
+            fwrite(&cliente, sizeof(struct Cliente), 1, arquivo);
+
+            // Fecha o arquivo
+            fclose(arquivo);
+
+            // Atualiza o extrato
+            extrato_arquivo = fopen("Extrato.bin", "ab");
+            fprintf(extrato_arquivo, "Data: %s | Deposito: + %.2f | Saldo: %.2f _ %s\n", data_e_hora_em_texto, valor, cliente.saldo, cpf);
+            fclose(extrato_arquivo);
+
+            printf("\nValor depositado!\n");
+            return;
+        }
+    }
+
+    printf("\nCPF inválido!\n");
+
+    // Fecha o arquivo
+    fclose(arquivo);
+}
+    FILE *arquivo, *extrato_arquivo;
+    struct Cliente cliente;
+
+    // Variáveis de data e hora
+    time_t t = time(NULL);
+    struct tm *data_hora_atual = localtime(&t);
+    char data_e_hora_em_texto[20];
+    strftime(data_e_hora_em_texto, sizeof(data_e_hora_em_texto), "%d/%m/%Y %H:%M", data_hora_atual);
+
+
+    // Abre o arquivo para leitura e escrita
+    arquivo = fopen("Clientes.bin", "rb+");
+//Verifica se o arquivo existe, caso nao exista exibe a mensagem de erro.
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo.\n");
         return;
@@ -362,7 +361,7 @@ void deposito(char cpf[16]){
 
             //Altera o saldo do cliente fazendo o deposito
             cliente.saldo += valor;
-        
+
             // Faz as alterações do arquivo
             fseek(arquivo, -sizeof(struct Cliente), SEEK_CUR);
             fwrite(&cliente, sizeof(struct Cliente), 1, arquivo);
@@ -372,18 +371,17 @@ void deposito(char cpf[16]){
 
             //abre o arquivo de extrato
             extrato_arquivo = fopen("Extrato.bin", "ab");
-            //Gera o extrato no arquivo de extrato
+//Gera o extrato no arquivo de extrato
             fprintf(extrato_arquivo, "Data: %s | Deposito: + %.2f | Saldo: %.2f _ %s\n", data_e_hora_em_texto, valor, cliente.saldo, cpf);
-            //fecha o arquivo de extrato
+//fecha o arquivo de extrato
             fclose(extrato_arquivo);
-
 
             printf("\nValor depositado!\n");
             return;
         }
     }
 
-    //Caso o cliente não exista , apresentará essa mensagem.
+//Caso o cliente não exista , apresentará essa mensagem.
     printf("\nCPF inválido!\n");
 
     // Fecha o arquivo
@@ -394,8 +392,6 @@ void transferencia(char cpf_origem[16], char senha_origem[5], char cpf_destino[1
     FILE *arquivo;
     struct Cliente cliente;
     struct Cliente cliente_origem, cliente_destino;
-
-
     // Variáveis de data e hora
     time_t t = time(NULL);
     struct tm *data_hora_atual = localtime(&t);
@@ -410,13 +406,13 @@ void transferencia(char cpf_origem[16], char senha_origem[5], char cpf_destino[1
 
     // Abre o arquivo para leitura e escrita
     arquivo = fopen("Clientes.bin", "r+");
-    //verifica se o arquivo existe, caso não exista, aparecerá uma mensagem de erro
+//verifica se o arquivo existe, caso não exista, aparecerá uma mensagem de erro
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo.\n");
         return;
     }
 
-    //variaveis para Verificar se os CPFs de origem e destino existem
+      //variaveis para Verificar se os CPFs de origem e destino existem
     int origem_encontrado = 0;
     int destino_encontrado = 0;
   
@@ -430,13 +426,13 @@ void transferencia(char cpf_origem[16], char senha_origem[5], char cpf_destino[1
             destino_encontrado = 1;
         }
     }
-    //Caso não exista cliente de origem ou senha estejaminvalida , aparecerá erro
+  //Caso não exista cliente de origem ou senha estejaminvalida , aparecerá erro
     if (!origem_encontrado) {
         printf("\nCPF de origem ou senha inválida!\n");
         fclose(arquivo);
         return;
     }
-    //Caso cliente de destino não exista , aparecerá erro
+  //Caso cliente de destino não exista , aparecerá erro
     if (!destino_encontrado) {
         printf("\nCPF de destino inválido!\n");
         fclose(arquivo);
@@ -488,9 +484,9 @@ void transferencia(char cpf_origem[16], char senha_origem[5], char cpf_destino[1
 
     // Gera o extrato no arquivo de extratos.
     FILE *extrato_arquivo;
-    //abre o arquivo de extrato
+//abre o arquivo de extrato
     extrato_arquivo = fopen("Extrato.bin", "ab");
-    //Cria os extratos no arquivo
+//Cria os extratos no arquivo
     fprintf(extrato_arquivo, "Data: %s | Tranferencia: - %.2f | Saldo:  %.2f _ %s\n", data_e_hora_em_texto, valor, cliente_origem.saldo, cpf_origem);
     fprintf(extrato_arquivo, "Data: %s | Tranferencia: + %.2f | Saldo:  %.2f _ %s\n", data_e_hora_em_texto, valor, cliente_destino.saldo, cpf_destino);
     fclose(extrato_arquivo);
@@ -498,3 +494,51 @@ void transferencia(char cpf_origem[16], char senha_origem[5], char cpf_destino[1
     printf("\nTransferência realizada com sucesso!\n");
 }
 
+void extratos(char cpf[16], char senha[5]) {
+    FILE *arquivo_cliente;
+    FILE *arquivo_extrato;
+    struct Cliente cliente;
+
+    arquivo_cliente = fopen("Clientes.bin", "rb");
+    if (arquivo_cliente == NULL) {
+        printf("Erro ao abrir o arquivo de clientes.\n");
+        return;
+    }
+
+    arquivo_extrato = fopen("Extrato.bin", "rb");
+    if (arquivo_extrato == NULL) {
+        printf("Erro ao abrir o arquivo de extrato.\n");
+        fclose(arquivo_cliente);
+        return;
+    }
+
+    char linha[MAX_STRING];
+
+    printf("\nExtratos:\n\n");
+
+    while (fread(&cliente, sizeof(struct Cliente), 1, arquivo_cliente)) {
+        if (strcmp(cliente.cpf, cpf) == 0 && strcmp(cliente.senha, senha) == 0) {
+            while (fgets(linha, MAX_STRING, arquivo_extrato) != NULL) {
+                // Verifica se a linha contém o CPF
+                if (strstr(linha, cpf) != NULL) {
+                    // Encontra a posição do caractere '_' na linha
+                    char *underline_pos = strchr(linha, '_');
+                    if (underline_pos != NULL) {
+                        // Calcula o tamanho da substring a ser impressa
+                        size_t len_to_print = underline_pos - linha;
+                        // Imprime a parte relevante da linha
+                        printf("%.*s\n", (int)len_to_print, linha);
+                    }
+                }
+            }
+
+            fclose(arquivo_extrato);
+            fclose(arquivo_cliente);
+            return;
+        }
+    }
+
+    fclose(arquivo_cliente);
+    fclose(arquivo_extrato);
+    return;
+}
